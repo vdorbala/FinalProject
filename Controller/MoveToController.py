21eb0aaeeffc5d62746baa58dab987a528a2792f
Ilyasem <ilbssemenov@gmail.com>
2019-12-10 17:51:24 -0500

    now includes yaw and move in one

--- Controller/MoveToController.py
+++ Controller/MoveToController.py
@@ -37,6 +37,11 @@ x=0
 y=0
 z=0
 
+yaw_error=0
+yaw_error_integral=0
+global_expected_yaw=0
+global_current_yaw=0
+
 
 islanded=False
 
@@ -101,6 +106,22 @@ def setlanded(msg):
     islanded=True
     pub_land.publish()
 
+    if islanded==False:
+        global_setpoint=data.z
+        global_expected_yaw=global_current_yaw+global_setpoint
+
+        if global_expected_yaw<-180:
+            global_expected_yaw=global_expected_yaw+360
+        if global_expected_yaw>180:
+            global_expected_yaw=global_expected_yaw-360
+
+
+        error=global_setpoint
+    else:
+        x=0
+        y=0
+        z=0
+        print('IGNORING DUE TO LANDED')
 def update_setpoint(data):
     global global_pos
     global global_waypoint
@@ -113,48 +134,73 @@ def update_setpoint(data):
     global followthrough
     global error_integral
 
+    global global_expected_yaw
+    global yaw_error
+    global yaw_error_integral
+
 
     if islanded==False:
         
-        if followthrough==False:
-
-            if data.w==1:
-                followthrough=True
+        if np.linalg.norm([data.x,data.y,data.z]) < 10:
+            if followthrough==False:
 
 
+                if data.w==0 or data.w==1:
+                    #okay we do a waypoint move
 
-            x = data.x # Positive fwd, body frame
-            y = data.y # Positive left, body frame
-            z = data.z # positive up, body frame
-            # IF Z WAS IN INERTIAL FRAME, RUN THE NEXT LINE
-            #    # transpose intertial coord z to body frame
-            #z = z - global_pos.position.z
+                    if data.w==1:
+                        followthrough=True
 
-            #okay so have a vector in the actual body frame, want to convert to inertial/odom frame
-            quat_B_to_I= np.array([global_pos.orientation.w, -global_pos.orientation.x, -global_pos.orientation.y, -global_pos.orientation.z])
-            quat_B_to_I_inv= np.array([global_pos.orientation.w, global_pos.orientation.x, global_pos.orientation.y, global_pos.orientation.z])
-            command_quat_body=np.array([0, x,y,z])
-            temp= quat_mult(quat_B_to_I_inv,command_quat_body)
-            command_quat_inertial= quat_mult(temp,quat_B_to_I)
-            command_vect_inertial= command_quat_inertial[1:]
 
-            #okay now have command in the odom frame, relative to body, so make it relative to origin
-            # Update the setpoint in global coordinates below
-            expected_pos_inertial= np.array([global_pos.position.x, global_pos.position.y, global_pos.position.z]) + command_vect_inertial
-            error= np.linalg.norm(command_vect_inertial)
-            error_integral=0
 
-            #publish it for RVIZ here if wanted
-            # global_waypoint.x = expected_pos_inertial[0]
-            # global_waypoint.y = expected_pos_inertial[1]
-            # global_waypoint.z = expected_pos_inertial[2]
-            # pub_waypoints.publish(global_waypoint)
+                    x = data.x # Positive fwd, body frame
+                    y = data.y # Positive left, body frame
+                    z = data.z # positive up, body frame
+                    # IF Z WAS IN INERTIAL FRAME, RUN THE NEXT LINE
+                    #    # transpose intertial coord z to body frame
+                    #z = z - global_pos.position.z
 
-            # Debugging, display body frame command
-            print('\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n') #clears the screen basically
-            print('Setpoint Updated (body frame): ',np.array([x,y,z,data.w]))
+                    #okay so have a vector in the actual body frame, want to convert to inertial/odom frame
+                    quat_B_to_I= np.array([global_pos.orientation.w, -global_pos.orientation.x, -global_pos.orientation.y, -global_pos.orientation.z])
+                    quat_B_to_I_inv= np.array([global_pos.orientation.w, global_pos.orientation.x, global_pos.orientation.y, global_pos.orientation.z])
+                    command_quat_body=np.array([0, x,y,z])
+                    temp= quat_mult(quat_B_to_I_inv,command_quat_body)
+                    command_quat_inertial= quat_mult(temp,quat_B_to_I)
+                    command_vect_inertial= command_quat_inertial[1:]
+
+                    #okay now have command in the odom frame, relative to body, so make it relative to origin
+                    # Update the setpoint in global coordinates below
+                    expected_pos_inertial= np.array([global_pos.position.x, global_pos.position.y, global_pos.position.z]) + command_vect_inertial
+                    error= np.linalg.norm(command_vect_inertial)
+                    error_integral=0
+
+                    #publish it for RVIZ here if wanted
+                    # global_waypoint.x = expected_pos_inertial[0]
+                    # global_waypoint.y = expected_pos_inertial[1]
+                    # global_waypoint.z = expected_pos_inertial[2]
+                    # pub_waypoints.publish(global_waypoint)
+
+                    # Debugging, display body frame command
+                    print('\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n') #clears the screen basically
+                    print('Setpoint Updated (body frame): ',np.array([x,y,z,data.w]))
+                else:
+                    #okay we yaw only
+                    followthrough=True
+
+                    global_expected_yaw=global_current_yaw+data.w
+
+                    if global_expected_yaw<-180:
+                        global_expected_yaw=global_expected_yaw+360
+                    if global_expected_yaw>180:
+                        global_expected_yaw=global_expected_yaw-360
+
+                    yaw_error= data.w
+                    yaw_error_integral=0
+
+            else:
+                print('IGNORING due to FOLLOWTHROUGH last command')
         else:
-            print('IGNORING FOLLOWTHROUGH')
+            print('FUCK YOU THATS TOO FAR')
     else:
         x=0
         y=0
@@ -172,91 +218,147 @@ def moveto_body():
     global error
     global followthrough
 
+    global global_current_yaw
+    global global_expected_yaw
+    global yaw_error_integral
+    global yaw_error
+
 
     acceptable_error=.1
     adaptive_threshold=.35
+
+    acceptable_yaw_error=2
 
     move_array=np.array([0.,0.,0.])
 
     if islanded==False:
         
-        print('Running right now with error: ',error)
 
-        if error>acceptable_error and islanded==False:
+        if yaw_error==0:
+            print('Running right now with error: ',error)
 
-            
-            current_pos_inertial=np.array([global_pos.position.x, global_pos.position.y, global_pos.position.z])
-            move_vect_inertial= expected_pos_inertial-current_pos_inertial
+            if error>acceptable_error and islanded==False:
 
-            #okay need to convert to vector in body frame to figure out where to move
-            #using current orientation global_pos in case something is flukey
-            quat_I_to_B= np.array([global_pos.orientation.w, global_pos.orientation.x, global_pos.orientation.y, global_pos.orientation.z])
-            quat_I_to_B_inv= np.array([quat_I_to_B[0], -quat_I_to_B[1], -quat_I_to_B[2],-quat_I_to_B[3]])
-            
-            move_quat_inertial= np.array([0, move_vect_inertial[0],  move_vect_inertial[1], move_vect_inertial[2]])
-            temp= quat_mult(quat_I_to_B_inv,move_quat_inertial)
-            move_quat_body= quat_mult(temp,quat_I_to_B)
-            move_vect_body= move_quat_body[1:]# this is basically your error vector
+                
+                current_pos_inertial=np.array([global_pos.position.x, global_pos.position.y, global_pos.position.z])
+                move_vect_inertial= expected_pos_inertial-current_pos_inertial
 
-            velocity_vect_body= np.array([global_vel.linear.x, global_vel.linear.y, global_vel.linear.z])
-            error_integral=error_integral+move_vect_body
+                #okay need to convert to vector in body frame to figure out where to move
+                #using current orientation global_pos in case something is flukey
+                quat_I_to_B= np.array([global_pos.orientation.w, global_pos.orientation.x, global_pos.orientation.y, global_pos.orientation.z])
+                quat_I_to_B_inv= np.array([quat_I_to_B[0], -quat_I_to_B[1], -quat_I_to_B[2],-quat_I_to_B[3]])
+                
+                move_quat_inertial= np.array([0, move_vect_inertial[0],  move_vect_inertial[1], move_vect_inertial[2]])
+                temp= quat_mult(quat_I_to_B_inv,move_quat_inertial)
+                move_quat_body= quat_mult(temp,quat_I_to_B)
+                move_vect_body= move_quat_body[1:]# this is basically your error vector
 
-            #move_vect_body[2]=1.29*move_vect_body[2]
+                velocity_vect_body= np.array([global_vel.linear.x, global_vel.linear.y, global_vel.linear.z])
+                error_integral=error_integral+move_vect_body
 
-
-            #ADAPTIVE GAINS FOR WHEN NEARBY
-
-            if error<adaptive_threshold:
-                print '----------adaptive gains active------------'
-                move_array[0]=.12*move_vect_body[0] - .25*velocity_vect_body[0] + .0015*error_integral[0] #TUNE THIS
-                move_array[1]=.12*move_vect_body[1] - .25*velocity_vect_body[1] + .0015*error_integral[1]
-                move_array[2]=.63*move_vect_body[2] - .15*velocity_vect_body[2] + .0015*error_integral[2]
-            else:
-                move_array[0]=.08*move_vect_body[0] - .16*velocity_vect_body[0] + .001*error_integral[0] #TUNE THIS
-                move_array[1]=.08*move_vect_body[1] - .16*velocity_vect_body[1] + .001*error_integral[1]
-                move_array[2]=.53*move_vect_body[2] - .10*velocity_vect_body[2] + .001*error_integral[2]
-
-            
-            print('error-------------------------------------------------------------------------')
-            print(error)
-            #timedelay= .1#TUNE THIS
-            # print('move vect')
-            # print(move_array)
-            # print('move_vect_body')
-            # print(move_vect_body)
-            # print('velocity_vect_body')
-            # print(velocity_vect_body)
-            # print('error_integral')
-            # print(error_integral)
-            # print(' ')
-            print('command is')
-            print(np.array([x,y,z]))
-            print('expected_pos_inertial')
-            print(expected_pos_inertial)
-            print('current_pos_inertial')
-            print(current_pos_inertial)
+                #move_vect_body[2]=1.29*move_vect_body[2]
 
 
+                #ADAPTIVE GAINS FOR WHEN NEARBY
 
-            global_command.linear.x=move_array[0]
-            global_command.linear.y=move_array[1]
-            global_command.linear.z=move_array[2]
-            global_command.angular.x=0
-            global_command.angular.y=0
-            global_command.angular.z=0
+                if error<adaptive_threshold:
+                    print '----------adaptive gains active------------'
+                    move_array[0]=.12*move_vect_body[0] - .25*velocity_vect_body[0] + .0015*error_integral[0] #TUNE THIS
+                    move_array[1]=.12*move_vect_body[1] - .25*velocity_vect_body[1] + .0015*error_integral[1]
+                    move_array[2]=.63*move_vect_body[2] - .15*velocity_vect_body[2] + .0015*error_integral[2]
+                else:
+                    move_array[0]=.08*move_vect_body[0] - .16*velocity_vect_body[0] + .001*error_integral[0] #TUNE THIS
+                    move_array[1]=.08*move_vect_body[1] - .16*velocity_vect_body[1] + .001*error_integral[1]
+                    move_array[2]=.53*move_vect_body[2] - .10*velocity_vect_body[2] + .001*error_integral[2]
+
+                
+                print('error-------------------------------------------------------------------------')
+                print(error)
+                #timedelay= .1#TUNE THIS
+                # print('move vect')
+                # print(move_array)
+                # print('move_vect_body')
+                # print(move_vect_body)
+                # print('velocity_vect_body')
+                # print(velocity_vect_body)
+                # print('error_integral')
+                # print(error_integral)
+                # print(' ')
+                print('command is')
+                print(np.array([x,y,z]))
+                print('expected_pos_inertial')
+                print(expected_pos_inertial)
+                print('current_pos_inertial')
+                print(current_pos_inertial)
 
 
 
-            pub_commands.publish(global_command)
-            error= np.linalg.norm(np.array([global_pos.position.x, global_pos.position.y, global_pos.position.z])-expected_pos_inertial)
+                global_command.linear.x=move_array[0]
+                global_command.linear.y=move_array[1]
+                global_command.linear.z=move_array[2]
+                global_command.angular.x=0
+                global_command.angular.y=0
+                global_command.angular.z=0
 
-            if error<acceptable_error and followthrough==True:
+
+
+                pub_commands.publish(global_command)
+                error= np.linalg.norm(np.array([global_pos.position.x, global_pos.position.y, global_pos.position.z])-expected_pos_inertial)
+
+                if error<acceptable_error and followthrough==True:
+                    followthrough=False
+
+        else:
+            print('YAW Running right now with error: ',yaw_error)
+
+            if abs(yaw_error)>acceptable_yaw_error and islanded==False:
+                print '---------------------'
+
+
+                yaw_error= global_expected_yaw-global_current_yaw
+
+                if yaw_error>180:
+                    yaw_error= -(360-yaw_error)
+                if yaw_error<-180:
+                    yaw_error= (360+yaw_error)
+
+                yaw_error_integral=yaw_error_integral+yaw_error
+
+                command = .008*yaw_error + .00003*yaw_error_integral
+
+                print('yaw error: ',yaw_error,' yaw  error_integral: ',yaw_error_integral,'  cmd: ',command)
+                print('current: ',global_current_yaw,' expected: ', global_expected_yaw)
+
+                
+                global_command.linear.x=0
+                global_command.linear.y=0
+                global_command.linear.z=0
+                global_command.angular.x=0
+                global_command.angular.y=0
+                global_command.angular.z=command
+
+
+
+                pub_commands.publish(global_command)
+
+                yaw_error= global_expected_yaw-global_current_yaw
+
+                if yaw_error>180:
+                    yaw_error= -(360-yaw_error)
+                if yaw_error<-180:
+                    yaw_error= (360+yaw_error)
+
+            if abs(yaw_error)<acceptable_yaw_error:
+                print 'finished yaw'
                 followthrough=False
+                yaw_error=0
 
     else:
         print('Landed')
         pub_land.publish()
         islanded=True
+
+
 
 
 if __name__ == '__main__':
